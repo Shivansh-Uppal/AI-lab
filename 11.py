28ea01cf63e72177911b20fe3b6dd941ae21a185
Shivansh uppal <74812139+Shivansh-Uppal@users.noreply.github.com>
2023-05-07 23:31:05 +0530

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,9 @@
+#Implement Brute force solution to the Knapsack problem in Python
+
+import nltk
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+text = "There have been centuries since i saw her. She was an old friend"
+print(sent_tokenize(text))
+print(word_tokenize(text))
+word_tokens = word_tokenize(text)
